1dbe4a0c616b5481ff3f0a7281112d06cdebb23e
Sayed Ali Husain <s0ali@outlook.com>
2023-01-26 14:21:01 +0300

    Play sound when the button is pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "Sounds/C", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
